05c32408c39a13ff2576cb9fee86f9e8e03e5009
Focus <hieunt9@gmail.com>
2022-04-27 12:25:40 +0700

    update 27042022

--- 03-aws/001-learn-terraform-aws-instance/main.tf
+++ 03-aws/001-learn-terraform-aws-instance/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "hit-test"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
