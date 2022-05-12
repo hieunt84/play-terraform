86632d62e66b05cbcbe3bd794ddad8bf302b75b0
Focus <hieunt9@gmail.com>
2022-05-12 18:46:51 +0700

    update 12May22

---
+++ 03-aws/002-learn-terrafrom-aws-iam/iam.tf
@@ -0,0 +1,46 @@
+//Ref: https://www.padok.fr/en/blog/aws-eks-cluster-terraform
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+//Ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
